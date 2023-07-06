af760a12763893a29cb84597a204534ec5ed28fb
duran-2020 <daduran12@gmail.com>
2023-07-06 17:38:30 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Animal List - list of animals and simply printing their names from the list"""
+
+def main():
+    
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant",]
+    
+    # display list1
+    print(list1)
+    
+main()
