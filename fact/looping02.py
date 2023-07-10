cfedd86da3a13cce6fcace36c9a2b1a8c68cb623
duran-2020 <daduran12@gmail.com>
2023-07-10 00:25:19 +0000

    "Looping techniques"

--- fact/looping02.py
+++ fact/looping02.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """Alta3 Research | RZFeeser
-   For - Looping across a file opened with 'with'"""
+   For - Using a file's lines as a source for the for-loop"""
 
 # open file in read mode
-with open("dnsservers.txt", "r") as dnsfile:
-    # indent to keep the dnsfile object open
-    # create list of lines
-    dnslist = dnsfile.readlines()
-    # loop over lines
-    for svr in dnslist:
-        #print and end without a newline
-        print(svr, end="")
+dnsfile = open("dnsservers.txt", "r")
 
-# no need to close our file - closed automatically
+# create list of lines
+dnslist = dnsfile.readlines()
+
+# loop over lines
+for svr in dnslist:
+    #print and end without a newline
+    print(svr, end="") # the line we read ALREADY contains a \n (newline)
+    
+# close the file (we created the list of lines)
+dnsfile.close() # best practice to close your file
 
